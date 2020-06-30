9880daae8a9e65c8aff725c9ecfc31f76b03ec88
pipeng119 <18610649031@163.com>
2020-06-30 22:25:06 +0800

    完成搜索页面

--- src/views/explore/Explore.vue
+++ src/views/explore/Explore.vue
@@ -6,13 +6,13 @@
         <span class="explore-header-search-icon"></span>
         <input class="explore-header-search-input font-24" type="text" placeholder="漫画名 | 作者 ^_^" />
       </div>
-      <span class="explore-header-btn" @click="search()">探索</span>
+      <span class="explore-header-btn font-30" @click="search()">搜索</span>
     </header>
     <section class="explore-recommend">
-      <div class="explore-recommend-title">
-        <span>大家都在搜</span>
-        <div class="explore-recommend-title">
-          <span class="explore-recommend-title-icon"></span>
+      <div class="explore-recommend-title font-26">
+        <div class="explore-recommend-title-text">大家都在搜</div>
+        <div class="explore-recommend-title-btn">
+          <span class="explore-recommend-title-btn-icon"></span>
           刷新
         </div>
       </div>
@@ -20,8 +20,8 @@
         <span
           class="explore-recommend-content-item"
           v-for="item in recommendList"
-          :key="item.title"
-        ></span>
+          :key="item"
+        >{{item}}</span>
       </div>
     </section>
   </div>
@@ -38,6 +38,20 @@ export default {
   },
   components: {
     GoBack
+  },
+  created() {
+    this.recommendList = [
+      "狂霸欧皇",
+      "惹上冷情BOSS",
+      "驭灵师",
+      "桃花宝典",
+      "很纯很美好",
+      "机长大人轻点爱",
+      "据说我是王的女儿",
+      "星辰变",
+      "霸情恶少：狂追小淘气",
+      "恶魔，别吻我"
+    ];
   },
   methods: {
     search() {
